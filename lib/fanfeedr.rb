1dfcc945eadfe2aebf1e0bd9ac68c98a1e4be891
Johnny Nguyen <johnnyn@gmail.com>
2009-10-09 10:38:30 -0500

    updated README

--- lib/fanfeedr.rb
+++ lib/fanfeedr.rb
@@ -6,12 +6,12 @@ require 'httparty'
 module Fanfeedr
 
 	def self.schedule(league, team)
-		response = HTTParty.get("http://api.fanfeedr.com/schedule?resource=team://#{league}/#{team}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :json)
+		response = HTTParty.get("http://api.fanfeedr.com/schedule?resource=team://#{league}/#{team}&appid=#{FANFEEDR_APPLICATION_ID}", :format => :json)
 	end
 	
 	def self.team_scores(league, team)
     response = []
-    results = HTTParty.get("http://api.fanfeedr.com/scores?resource=team://#{league}/#{team}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :xml)["response"]["result"]["doc"]
+    results = HTTParty.get("http://api.fanfeedr.com/scores?resource=team://#{league}/#{team}&appid=#{FANFEEDR_APPLICATION_ID}", :format => :xml)["response"]["result"]["doc"]
 
     results.each do |r|
       score_details = CGI::parse(r['str'][0])
@@ -23,7 +23,7 @@ module Fanfeedr
 
 	def self.league_scores(league)
     response = []	
-    results = HTTParty.get("http://api.fanfeedr.com/scores?resource=league://#{league}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :xml)["response"]["result"]["doc"]
+    results = HTTParty.get("http://api.fanfeedr.com/scores?resource=league://#{league}&appid=#{FANFEEDR_APPLICATION_ID}", :format => :xml)["response"]["result"]["doc"]
 
     results.each do |r|
       score_details = CGI::parse(r['str'][0])
@@ -34,15 +34,15 @@ module Fanfeedr
 	end
 	
 	def self.team_resource_feed(league, team)
-		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=team://#{league}/#{team}&n&appid=zpeyk4taevn7rc9en7j34zc4&format=json", :format => :json)
+		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=team://#{league}/#{team}&n&appid=#{FANFEEDR_APPLICATION_ID}&format=json", :format => :json)["docs"]
 	end
 
 	def self.league_resource_feed(league)	
-		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=league://#{league}&appid=zpeyk4taevn7rc9en7j34zc4&format=json", :format => :json)
+		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=league://#{league}&appid=#{FANFEEDR_APPLICATION_ID}&format=json", :format => :json)["docs"]
 	end
 	
 	def self.get_article(resource)	
-		response = HTTParty.get("http://api.fanfeedr.com/get_article?resource=article://#{resource}&appid=zpeyk4taevn7rc9en7j34zc4")
+		response = HTTParty.get("http://api.fanfeedr.com/get_article?resource=article://#{resource}&appid=#{FANFEEDR_APPLICATION_ID}")
 	end
 
 end
