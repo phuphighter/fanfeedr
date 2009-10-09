49bb1e9e5b677003a90ef0339c84e037bc73129f
Johnny Nguyen <johnnyn@gmail.com>
2009-10-09 09:23:29 -0500

    initial commit

---
+++ lib/fanfeedr.rb
@@ -0,0 +1,32 @@
+# Fanfeedr
+require 'rubygems'
+gem 'httparty'
+require 'httparty'
+
+module Fanfeedr
+
+	def self.schedule(league, team)
+		response = HTTParty.get("http://api.fanfeedr.com/schedule?resource=team://#{league}/#{team}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :json)
+	end
+	
+	def self.team_scores(league, team)
+		response = HTTParty.get("http://api.fanfeedr.com/scores?resource=team://#{league}/#{team}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :xml)["response"]["result"]["doc"]
+	end
+
+	def self.league_scores(league)	
+		response = HTTParty.get("http://api.fanfeedr.com/scores?resource=league://#{league}&appid=zpeyk4taevn7rc9en7j34zc4", :format => :xml)["response"]["result"]["doc"]
+	end
+	
+	def self.team_resource_feed(league, team)
+		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=team://#{league}/#{team}&n&appid=zpeyk4taevn7rc9en7j34zc4&format=json", :format => :json)
+	end
+
+	def self.league_resource_feed(league)	
+		response = HTTParty.get("http://api.fanfeedr.com/resource_feed?resource=league://#{league}&appid=zpeyk4taevn7rc9en7j34zc4&format=json", :format => :json)
+	end
+	
+	def self.get_article(resource)	
+		response = HTTParty.get("http://api.fanfeedr.com/get_article?resource=article://#{resource}&appid=zpeyk4taevn7rc9en7j34zc4")
+	end
+
+end
---
+++ tasks/fanfeedr_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :fanfeedr do
+#   # Task goes here
+# end
